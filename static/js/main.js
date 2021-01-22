f8a094c9c62049f009c8a052a4d5e3f4f6d01788
Ruben Villalvazo <vim.ruben@hotmail.com>
2021-01-22 13:38:08 -0300

    Ajuste de funçao de dificuldade

--- static/js/main.js
+++ static/js/main.js
@@ -48,6 +48,90 @@ function game_over() {
     } 
 }
 
+function dificultad() {
+    if (score >= 0 && score <= 5) {
+        gap = 85;
+        gravity = 1.1;
+        intervalo_tubos = 150
+    } else if (score > 5 && score <= 10) {
+        gap = 90;
+        gravity = 1.3;
+        intervalo_tubos = 175;
+    } else if (score >= 10 && score <= 15) {
+        gap = 95;
+        gravity = 1.5;
+        intervalo_tubos = 200;
+    } else if (score >= 15 && score <= 20) {
+        gap = 100;
+        gravity = 1.7;
+        intervalo_tubos = 225;
+    } else if (score >= 20 && score <= 25) {
+        gap = 105;
+        gravity = 1.7;
+        intervalo_tubos = 250;
+    } else if (score >= 25 && score <= 30) {
+        gap = 110;
+        gravity = 1.9;
+        intervalo_tubos = 275;
+    } else if (score == 31){
+        gap = 115;
+        gravity = 1.9;
+        intervalo_tubos = 125;
+    } else if (score > 31 && score <=35 ){
+        gap = 115;
+        gravity = 1.9;
+        intervalo_tubos = 300;
+    } else if (score == 36) {
+        gap = 120;
+        gravity = 2.1;
+        intervalo_tubos = 125;
+    } else if (score > 36 && score <= 40) {
+        gap = 120;
+        gravity = 2.1;
+        intervalo_tubos = 325;
+    } else if (score == 41) {
+        gap = 115;
+        gravity = 2.1;
+        intervalo_tubos = 125;
+    } else if (score > 41 && score <= 45) {
+        gap = 110;
+        gravity = 2.1;
+        intervalo_tubos = 350;
+    } else if (score == 46) {
+        gap = 105;
+        gravity = 2.1;
+        intervalo_tubos = 125;
+    } else if (score > 46 && score <= 50) {
+        gap = 100;
+        gravity = 2.1;
+        intervalo_tubos = 375;
+    } else if (score == 51) {
+        gap = 95;
+        gravity = 2.1;
+        intervalo_tubos = 125;
+    } else if (score > 51 && score <= 55) {
+        gap = 90;
+        gravity = 2.1;
+        intervalo_tubos = 400;
+    } else if (score == 56) {
+        gap = 85;
+        gravity = 2.2;
+        intervalo_tubos = 125;
+    } else if (score > 56 && score <= 60) {
+        gap = 90;
+        gravity = 2.3;
+        intervalo_tubos = 425;
+    } else if (score == 61) {
+        gap = 95;
+        gravity = 2.5;
+        intervalo_tubos = 125;
+    } else if (score > 61) {
+        gap = 100;
+        gravity = 2.5;
+        intervalo_tubos = 450;
+    }
+}
+
 function posicion_tubos(min, max) {
     return Math.random() * (max - min) + min;
 }
@@ -97,75 +181,7 @@ function draw(){
             scor.play();
         }
 
-        if (score >= 0 && score <= 5) {
-            gap = 85;
-            gravity = 1.1;
-            intervalo_tubos = 150
-        } else if (score > 5 && score <= 10) {
-            gap = 90;
-            gravity = 1.3;
-            intervalo_tubos = 175;
-        } else if (score >= 10 && score <= 15) {
-            gap = 95;
-            gravity = 1.5;
-            intervalo_tubos = 200;
-        } else if (score >= 15 && score <= 20) {
-            gap = 100;
-            gravity = 1.7;
-            intervalo_tubos = 225;
-        } else if (score >= 20 && score <= 25) {
-            gap = 105;
-            gravity = 1.7;
-            intervalo_tubos = 250;
-        } else if (score >= 25 && score <= 30) {
-            gap = 110;
-            gravity = 1.9;
-            intervalo_tubos = 275;
-        } else if (score >= 30 && score <=35 ){
-            gap = 115;
-            gravity = 1.9;
-            intervalo_tubos = 287;
-        } else if (score >= 35 && score <= 40) {
-            gap = 120;
-            gravity = 2.1;
-            intervalo_tubos = 300;
-        } else if (score >= 40 && score <= 45) {
-            gap = 115;
-            gravity = 2.1;
-            intervalo_tubos = 301;
-        } else if (score >= 45 && score <= 47) {
-            gap = 110;
-            gravity = 2.1;
-            intervalo_tubos = 302;
-        } else if (score >= 47 && score <= 49) {
-            gap = 105;
-            gravity = 2.1;
-            intervalo_tubos = 302;
-        } else if (score >= 49 && score <= 51) {
-            gap = 100;
-            gravity = 2.1;
-            intervalo_tubos = 302;
-        } else if (score >= 51 && score <= 53) {
-            gap = 95;
-            gravity = 2.1;
-            intervalo_tubos = 302;
-        } else if (score >= 53 && score <= 55) {
-            gap = 90;
-            gravity = 2.1;
-            intervalo_tubos = 302;
-        } else if (score >= 55 && score <= 57) {
-            gap = 85;
-            gravity = 2.2;
-            intervalo_tubos = 302;
-        } else if (score >= 57 && score <= 59) {
-            gap = 85;
-            gravity = 2.3;
-            intervalo_tubos = 302;
-        } else if (score >= 60) {
-            gap = 85;
-            gravity = 2.5;
-            intervalo_tubos = 302;
-        }
+        dificultad();
 
         if (pipe[i].x == intervalo_tubos) {
             pipe.push({
