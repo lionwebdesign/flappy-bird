16f89e53e8b2dc92744f069a83072a53fd259bac
Ruben Villalvazo <vim.ruben@hotmail.com>
2021-01-30 13:34:15 -0300

    Sin estructura blueprint para comprobar errores

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-from flappy_bird.flappy_bird import flappy_bird
+#from flappy_bird.flappy_bird import flappy_bird
 
 app = Flask(__name__)
 
-app.register_blueprint(flappy_bird, url_prefix='/flappy_bird')
+#app.register_blueprint(flappy_bird, url_prefix='/flappy_bird')
 
 @app.route('/')
 def home():
