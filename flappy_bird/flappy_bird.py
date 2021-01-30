69e42120d18ee39692fd578d44b98bccf960d13a
Ruben Villalvazo <vim.ruben@hotmail.com>
2021-01-30 13:33:31 -0300

    Sin estructura blueprint para comprobar errores

--- flappy_bird/flappy_bird.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-flappy_bird = Blueprint('flappy_bird', __name__)
-
-@flappy_bird.route('/')
-def index():
-    return render_template('flappy_bird.html')
