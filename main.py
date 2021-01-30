8a7f2233c647f07b58b83c2a57c11dc4694e743f
Ruben Villalvazo <vim.ruben@hotmail.com>
2021-01-30 12:04:28 -0300

    Correccion de ruta de aplicacion

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from flappy_bird.flappy_bird import flappy_bird
 
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return render_template('componentes/home.html')
 
 app.register_blueprint(flappy_bird, url_prefix='/flappy_bird')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
